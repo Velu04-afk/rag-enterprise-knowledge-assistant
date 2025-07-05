fdd0d891d93ccb815d60c32d49f316a07965246f
Velu04-afk <velmurgansanthanam@gmail.com>
2025-07-05 12:54:28 +0530

    fix: removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import cohere
-
-co = cohere.ClientV2("wJjhtebfQ08cRFOoMNJFqW1jyZZbH1LyWTy51acu")
-response = co.chat(
-    model="command-a-03-2025",
-    messages=[{"role": "user", "content": "hello world!"}]
-)
-
-print(response)
